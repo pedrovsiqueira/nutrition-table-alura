11503d1c01b8658882370a24eac76e3c48599ced
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-01-23 19:16:50 -0300

    Finalizando AJAX

--- js/form.js
+++ js/form.js
@@ -3,27 +3,27 @@ botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault(); //bloquear comportamento padrão
 
     var form = document.querySelector("#form-adiciona");
-    //extraindo informações do paciente do form
     var paciente = obtemPacienteDoFormulario(form);
 
-    //cria a tr e a td do paciente
-    var pacienteTr = montaTR(paciente);
-
     var erros = validaPaciente(paciente);
-
     if (erros.length > 0) {
         exibeMensagensDeErro(erros);
         return;
     }
 
-    //colocar o paciente dentro da tabela
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
-})
+});
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTR(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+}
 
 function exibeMensagensDeErro(erros){
     var ul = document.querySelector("#mensagens-erro")
